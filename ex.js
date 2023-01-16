6233686ae4220a5c9cedf948f21989e895b5e434
EleanorColvin <99199758+EleanorColvin@users.noreply.github.com>
2023-01-16 14:57:21 -0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -1,33 +1,26 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
-app.get('/', (req, res) => {
+app.get('/', (req,res)=>{
     res.send('Hello there');
-})
+});
 const courses = [
-    { id: 1, name: 'Web Development' },
-    { id: 2, name: 'IT' },
-    { id: 3, name: 'Cybersecurity' },
+    {id: 1, name:'Web Development'},
+    {id:2, name: 'IT'},
+    {id:3, name: 'Cybersecurity'},
 ];
-// HTTP GET REQUESTS
-app.get('/api/courses', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send("The course with the givern ID was not found");
+// http GET requests route
+app.get('/api/courses',(req, res)=>{
+    res.send(courses);
+});
+//request course by id 
+app.get('/api/courses/:id',(req, res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course){
+        res.status(404).send("The course with the given ID was not found");
+        return
     }
     res.send(course);
 })
-//HTTP POST REQUESTS
-app.post('api/courses', (req, res) => {
-    if (course.length <= 3) {
-        const course = {
-
-            id: course.length + 1,
-            name: req.body.name
-        }
-    }
-        res.push(course);
-    })
-app.listen(300, () => {
-    console.log('Listening on port 3000...')
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
 })
